2fe8906cd62994126d6d253b7120cd14ce3dfe29
Jun Kawahara <junkawahara@users.noreply.github.com>
2019-08-30 13:48:14 +0900

    Update template.cpp

--- template.cpp
+++ template.cpp
@@ -12,10 +12,33 @@ using namespace sbddh;
 
 int main() {
 
+    // SAPPOROBDD functions
+
     BDD_Init(1024, 1024 * 1024 * 1024);
     BDD_NewVar();
+    BDD_NewVar();
+
+    ZBDD z1 = ZBDD(1);
+    z1 = z1.Change(1);
+
+    // SAPPOROBDD helper functions
+    ZBDD z2 = getSingleton(1);
+
+    //std::cout << (z1 == z3 ? "z1 == z3" : "z1 != z3") << std::endl;
+
+    ZBDD z3 = getChild1(z2);
+
+    //std::cout << "z3 is" << (isConstant(z3) ? " " : " not ") <<
+    //    "constant" << std::endl;
+
+    // tdzdd functions
 
     DdStructure<2> dd;
+    NodeId node = dd.root();
+
+    bool b = (z1 == z2 && isConstant(z3) && node.row() == 0 && node.col() == 0);
+    std::cout << "program " << (b ? "works" : "does not work")
+              << " correctly" << std::endl;
 
     return 0;
 }
